bd9c70862a9061555eb717b9339897d7bd8b0ef7
snielchevallier <snielchevallier@gmail.com>
2026-06-22 19:15:05 +0200

    integration forms login et signin

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,7 +1,49 @@
 export default function LoginForm() {
   return (
-    <div className="border border-dashed border-grey-dark p-4">
-      <span className="text-sm text-grey-dark">Je suis le LoginForm</span>
-    </div>
+    <form className="w-90 mx-auto mt-4 gap-4 flex flex-col">
+      <div className="mb-2">
+        <label htmlFor="email" className="block text-sm font-normal text-black">
+          Adresse email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          className="mt-1 p-2 block w-full
+                border border-grey-light rounded-sm
+                text-sm font-normal
+                focus:outline-none focus:ring-2 focus:ring-main-red focus:ring-offset-2"
+          placeholder="Votre adresse email"
+        />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="password" className="block text-sm font-normal text-black">
+          Mot de passe
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          className="mt-1 p-2 block w-full
+                border border-grey-light rounded-sm
+                text-sm font-normal
+                focus:outline-none focus:ring-2 focus:ring-main-red focus:ring-offset-2"
+          placeholder="Votre mot de passe"
+        />
+      </div>
+      <button
+        type="submit"
+        className="
+              w-full py-2 px-4 
+              bg-main-red text-white text-base font-medium
+              rounded-xl 
+              hover:bg-orange/90 
+              focus:outline-none focus:ring-2 focus:ring-main-red focus:ring-offset-2"
+      >
+        Se connecter
+      </button>
+    </form>
   )
 }
--- src/components/forms/SigninForm.tsx
+++ src/components/forms/SigninForm.tsx
@@ -1,7 +1,89 @@
+import Link from 'next/link'
+
 export default function SigninForm() {
   return (
-    <div className="border border-dashed border-grey-dark p-4">
-      <span className="text-sm text-grey-dark">Je suis le SigninForm (inscription)</span>
-    </div>
+    <form className="w-90 mx-auto mt-4 gap-4 flex flex-col">
+      <div className="mb-2">
+        <label htmlFor="email" className="block text-sm font-normal text-black">
+          Nom
+        </label>
+        <input
+          type="text"
+          id="lastname"
+          name="lastname"
+          required
+          className="mt-1 p-2 block w-full
+                border border-grey-light rounded-sm
+                text-sm font-normal
+                focus:outline-none focus:ring-2 focus:ring-main-red focus:ring-offset-2"
+          placeholder="Votre nom"
+        />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="email" className="block text-sm font-normal text-black">
+          Prénom
+        </label>
+        <input
+          type="text"
+          id="firstname"
+          name="firstname"
+          required
+          className="mt-1 p-2 block w-full
+                border border-grey-light rounded-sm
+                text-sm font-normal
+                focus:outline-none focus:ring-2 focus:ring-main-red focus:ring-offset-2"
+          placeholder="Votre prénom"
+        />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="email" className="block text-sm font-normal text-black">
+          Adresse email
+        </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          required
+          className="mt-1 p-2 block w-full
+                border border-grey-light rounded-sm
+                text-sm font-normal
+                focus:outline-none focus:ring-2 focus:ring-main-red focus:ring-offset-2"
+          placeholder="Votre adresse email"
+        />
+      </div>
+      <div className="mb-2">
+        <label htmlFor="password" className="block text-sm font-normal text-black">
+          Mot de passe
+        </label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          required
+          className="mt-1 p-2 block w-full
+                border border-grey-light rounded-sm
+                text-sm font-normal
+                focus:outline-none focus:ring-2 focus:ring-main-red focus:ring-offset-2"
+          placeholder="Votre mot de passe"
+        />
+      </div>
+      <div className="mb-2">
+        <input type="checkbox" id="terms" name="terms" required className="mr-2" />
+        <label htmlFor="terms" className="text-sm text-black">
+          J'accepte les <Link href="/cgu" target="_blank" className="text-main-red hover:underline">conditions générales d'utilisation</Link>
+        </label>
+      </div>
+      <button
+        type="submit"
+        className="
+              w-full py-2 px-4 
+              bg-main-red text-white text-base font-medium
+              rounded-xl 
+              hover:bg-orange/90 
+              focus:outline-none focus:ring-2 focus:ring-main-red focus:ring-offset-2"
+      >
+        S'inscrire
+      </button>
+    </form>
   )
 }
